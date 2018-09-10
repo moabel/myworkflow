6379bc1e247055feb203079b296e912e93312ad5
SupahJay <37572525+SupahJay@users.noreply.github.com>
2018-09-10 15:50:58 -0300

    Update Core.js

--- core.js
+++ core.js
@@ -1,23 +1,20 @@
-window.onload = () => {
-            loadImages()
-        }
-        window.onresize = () => {
-            loadImages()
-        }
-
-        function loadImages() {
-            document.querySelectorAll(".img").forEach((x) => {
-                console.log(x);
-                x.style.background = "tranparent";
-                var yey = "url(https://picsum.photos/" + x.offsetWidth + "/?random)";
-                x.style.backgroundImage = yey;
-            })
-        }
-        window.onclick = () => {
-            console.clear()
-            var sup = document.childNodes;
-            var varr = "";
-            //var newe = sup.forEach((xx)=>{varr += String(xx.nodelist)})
-            console.log(sup)
-            console.log(newe)
-        }
+window.onload = loadImages;
+window.onresize = loadImages;
+altStyles=["gray"]
+function loadImages() {
+    document.querySelectorAll(".img").forEach((x) => {
+        var g="";
+        altStyles.forEach(y=>x.classList.contains(y)? g="g":null)
+        x.style.background = "tranparent";
+        var bgm = "https://picsum.photos/"+ g +"/" + x.offsetWidth + "/"+x.offsetHeight+ "/?random";
+        x.tagName=="IMG"?x.src=bgm:x.style.backgroundImage = "url("+bgm+")";
+    })
+}
+//         window.onclick = () => {
+//             console.clear()
+//             var sup = document.childNodes;
+//             var varr = "";
+//             //var newe = sup.forEach((xx)=>{varr += String(xx.nodelist)})
+//             console.log(sup)
+//             console.log(newe)
+//         }
