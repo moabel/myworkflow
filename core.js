d06147bc40a7831aa401a243cd3dbf51dc58d028
SupahJay <37572525+SupahJay@users.noreply.github.com>
2018-10-13 19:04:40 -0300

    Update core.js

--- core.js
+++ core.js
@@ -3,20 +3,21 @@ const myflow = (function(){
 	window.addEventListener("resize", loadImages);
 	altStyles=["gray"];
 	//altStyles refere á tags com estilos diferentes, até agora só criei cinza mas o código é fácil de acrescentar mais outras com só uma linha de código
+
 	function loadImages() {
 		document.querySelectorAll(".img").forEach((x) => {
 			var g="";
-			if ( altStyles.forEach(y=>x.classList.contains(y) ) { g = "g" })
+			if ( altStyles.forEach(y=>x.classList.contains(y) ) ) { g = "g" };
 			x.style.background = "tranparent";
 			//O uso de ""+"" foi escolhido ao invéz de ${} simplismente por ser mais compativel
 			var bgm = "https://picsum.photos/"+ g +"/" + x.offsetWidth + "/"+x.offsetHeight+ "/?random";
 			x.tagName == "IMG" ? x.src = bgm : x.style.backgroundImage = "url("+bgm+")";
 		})
 	}
-    return {
-        watch : function ( targetNode ) {
+	
+	methods = { watch : function ( targetNode ) {
         var event = new MutationObserver( loadImages );
         event.observe( targetNode, { childList: true } );
-        }
-    }
+        }}
+    return methods
 })()
