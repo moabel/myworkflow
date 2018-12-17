5ed10718123167f607751fdc1633803958088d7c
unknown <moabelimagomes@hotmail.com>
2018-12-16 22:26:33 -0300

    Efficiency update

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # My Workflow
-I'm posting this here so I can stop Copy and Paste'ing code in my workflow.
+Code to speed up my workflow.
+Creates content that changes on every page load and simulates user generated random content so you can care with what matters in the building time.
+When coding only include this tag to your HTML and everything will work just like that.
+*No dependencies
+
+### Includes:
+- Dummy image placeholder that just fit the size of any tag
+- DOM change observer that reloads/adds images whenever necessary
+
+### More to come:
+[ ] Dummy headlines and content generator
+[ ] Non default options(not reload images on resize, defined sizes)
+
+## Just include this tag on your code
+Every image must have a .img class.
 ```
 <script src="https://cdn.rawgit.com/SupahJay/myworkflow/master/core.js"></script>
 ```
+ | Feature | Usage | Notes |
+ | ------- | ----- | - |
+ | Dummy Image generator | Give any <img> or any tag the .img tag | This will give a source(src) to <img>s but for any other type of tag it will add a background-image|
+ | Dom change Observer | watch(nodeName [, callback]) | With no callbacks this function watches for dom changes to load more images when when a new tag appears|
--- core.js
+++ core.js
@@ -1,21 +1,18 @@
 window.onload = loadImages;
 window.onresize = loadImages;
 function loadImages() {
-    var valuess= "";
-    var altStyles= ["gray"];
-    var altSizeCounter= 1;
+    var sizeHistory = [], altStyles = ["gray"], altSizeCounter = 1;
+    
     document.querySelectorAll(".img").forEach( ( imgClass ) => {
-        var g="", num  = "/?random";
-        if ( imgClass.classList.value.includes(...altStyles) ){ g = "g" }
-        var size = imgClass.offsetWidth + "/" + imgClass.offsetHeight + "";
-        valuess = [...valuess, size]
-        if ( valuess.includes(size) ){ num ="/?images=" + altSizeCounter++ }
-        imgClass.style.background = "tranparent";
-        var bgm = "https://picsum.photos/" + (g +"/") + size + num;
+        var g="", num  = "/?random", size = imgClass.offsetWidth + "/" + imgClass.offsetHeight;
+        if ( imgClass.classList.value.includes(...altStyles) ){ g = "g/"}
+        if ( sizeHistory.includes(size) ){ num ="/?images=" + altSizeCounter++ }
+        else { sizeHistory.push(size) }
+        var bgm = "https://picsum.photos/" + g + size + num;
         imgClass.tagName=="IMG"? imgClass.src = bgm : imgClass.style.backgroundImage = "url(" + bgm + ")";
     })
 }
-function watch(targetNode){
-    var event=new MutationObserver(loadImages);
+function watch(targetNode,callback){
+    var event = new MutationObserver(callback || loadImages);
     event.observe(targetNode, { childList: true });
 }
